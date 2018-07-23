c7aefa6c8a3d88fc9add46f1da65ebb904e98863
Scott Revell <srevell@whitepenny.com>
2018-07-23 15:21:20 -0400

    Removed Unused Requires

--- wp-content/themes/jerseyman/lib/script.js
+++ wp-content/themes/jerseyman/lib/script.js
@@ -1,14 +1,3 @@
-//=require picturefill/dist/picturefill.min.js
-
-//=require owl.carousel/dist/owl.carousel.min.js
-
-//=require formstone/dist/js/core.js
-//=require formstone/dist/js/background.js
-//=require formstone/dist/js/checkpoint.js
-//=require formstone/dist/js/mediaquery.js
-//=require formstone/dist/js/navigation.js
-//=require formstone/dist/js/swap.js
-//=require formstone/dist/js/transition.js
 
 document.createElement( "picture" );
 
